ebdf382d379e238539fd74acfa33f8ccb873ee86
Hritik Arora <hritikarora875@gmail.com>
2021-06-10 20:21:09 +0530

    6th_commit

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -5,7 +5,7 @@ const BlogContext = React.createContext();
 export const BlogProvider = ({ children }) => {
     const [blogPosts, setBlogPosts] = useState([])
     const addBlogPost=()=>{
-        setBlogPosts([...blogPosts,{titile:`Blog Post ${blogPosts.length + 1}`}]);
+        setBlogPosts([...blogPosts,{title:`Blog Post ${blogPosts.length + 1}`}]);
     }
     return (
     <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
