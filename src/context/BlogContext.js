d5244bcd4c9446f9e36b8ccea85bb2cc68f3cbe3
Hritik Arora <hritikarora875@gmail.com>
2021-06-17 20:53:11 +0530

    11th commit

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,23 +1,18 @@
-
 import createDataContext from './createDataContext';
 
+const blogReducer = (state, action) => {
+	switch (action.type) {
+		case 'add_blogpost':
+			return [...state, { id: Math.floor(Math.random() * 99999), title: `blog post #${state.length + 1}` }];
 
-const blogReducer = (state,action) =>{
-    switch (action.type) {
-        case 'add_blogpost':
-            return [...state, {title:`blog post #${state.length+1}`}];
-            
-        default:
-            return state;
-        
-    }
-}
-const addBlogPost = (dispatch) =>{
-    return () =>{
-    dispatch({type:'add_blogpost'});
-    }
+		default:
+			return state;
+	}
+};
+const addBlogPost = (dispatch) => {
+	return () => {
+		dispatch({ type: 'add_blogpost' });
+	};
 };
 
-export const {Context,Provider} = createDataContext(blogReducer,{addBlogPost},[]);
-
- 
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost }, []);
