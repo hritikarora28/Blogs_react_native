e273a64bdd53bf5cfeb6f1423f260419b9f87cb3
Hritik Arora <hritikarora875@gmail.com>
2021-06-28 22:05:01 +0530

    16th commit

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,17 +1,35 @@
-import React,{useContext} from 'react';
-import { View,Text,StyleSheet }from 'react-native';
+import React, { useContext, useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
 
-const CreateScreen = ({navigation}) =>{
-    return (
-    <View>
-        <Text>Create</Text>
-    </View>
+const CreateScreen = ({ navigation }) => {
+	const [title, setTitle] = useState('');
+	const [content, setContent] = useState('');
+	return (
+		<View>
+			<Text style={styles.label}>Enter Title:</Text>
+			<TextInput style={styles.input} value={title} onChange={(text) => setTitle(text)} />
+			<Text style={styles.label}>Enter Content: </Text>
+			<TextInput style={styles.input} value={content} onChange={(text) => setContent(text)} />
+            <Button title="Add Blog Post"/>
+		</View>
+	);
+};
 
-    );  
-}
+const styles = StyleSheet.create({
+	input: {
+		fontSize: 18,
+		borderWidth: 1,
+		borderColor: 'black',
+		marginBottom: 15,
+		padding: 5,
+		margin: 5,
+	},
+	label: {
+		fontSize: 20,
+		marginBottom: 5,
+		marginLeft: 5,
+	},
+});
 
-const styles = StyleSheet.create({});
-
-
-export default  CreateScreen;
+export default CreateScreen;
