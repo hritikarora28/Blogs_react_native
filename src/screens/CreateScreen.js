0a11aace681eb0f3b1e49409b5b688df5350001e
Hritik Arora <hritikarora875@gmail.com>
2021-07-02 14:06:18 +0530

    18th commit

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -5,14 +5,21 @@ import { Context } from '../context/BlogContext';
 const CreateScreen = ({ navigation }) => {
 	const [title, setTitle] = useState('');
 	const [content, setContent] = useState('');
-	const {addBlogPost} =useContext(Context);
+	const { addBlogPost } = useContext(Context);
 	return (
 		<View>
 			<Text style={styles.label}>Enter Title:</Text>
 			<TextInput style={styles.input} value={title} onChangeText={(text) => setTitle(text)} />
 			<Text style={styles.label}>Enter Content: </Text>
 			<TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)} />
-            <Button title="Add Blog Post" onPress={()=> addBlogPost(title,content)}/>
+			<Button
+				title="Add Blog Post"
+				onPress={() => {
+					addBlogPost(title, content, () => {
+						navigation.navigate('Index');
+					});
+				}}
+			/>
 		</View>
 	);
 };
