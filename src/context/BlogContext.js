f3a519626fd4a6c6b13a789838fe33a8f8303ae4
Hritik Arora <hritikarora875@gmail.com>
2021-08-21 16:38:56 +0530

    27th commit

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,34 +1,54 @@
-import createDataContext from './createDataContext';
+import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
-	switch (action.type) {
-		case 'delete_blogpost':
-			return state.filter((blogPost) => blogPost.id != action.playload);
-		case 'add_blogpost':
-			return [
-				...state,
-				{ id: Math.floor(Math.random() * 99999), title: action.payload.title, content: action.payload.content },
-			];
+  switch (action.type) {
+    case "edit_blogpost":
+      return state.map((blogPost) => {
+        return blogPost.id === action.playload.id ? action.playload : blogPost;
+      });
 
-		default:
-			return state;
-	}
+    case "delete_blogpost":
+      return state.filter((blogPost) => blogPost.id != action.playload);
+    case "add_blogpost":
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: action.payload.title,
+          content: action.payload.content,
+        },
+      ];
+
+    default:
+      return state;
+  }
 };
 const addBlogPost = (dispatch) => {
-	return (title, content, callback) => {
-		dispatch({ type: 'add_blogpost', payload: { title, content } });
-		callback();
-	};
+  return (title, content, callback) => {
+    dispatch({ type: "add_blogpost", payload: { title, content } });
+    if (callback){
+	callback();
+	}
+  };
 };
 const deleteBlogPost = (dispatch) => {
-	return (id) => {
-		dispatch({ type: 'delete_blogpost', playload: id });
-	};
+  return (id) => {
+    dispatch({ type: "delete_blogpost", playload: id });
+  };
 };
-const editBlogPost = (dispatch) =>{
-	return (id, title, content) =>{
-		dispatch({type:'edit_blogpost', playload:{id, title, content}})
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({ type: "edit_blogpost", playload: { id, title, content } });
+	
+	if (callback){
+	callback();
 	}
-}
+  
+};
+};
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost , editBlogPost}, [{title:'Test Post', content:'TEST CONTENT', id:1}]);
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost, deleteBlogPost, editBlogPost },
+  [{ title: "Test Post", content: "TEST CONTENT", id: 1 }]
+);
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -4,20 +4,23 @@ import {Context} from '../context/BlogContext';
 import BlogPostForm from "../componets/blogPostForm";
 
 const EditScreen = ({ navigation}) =>{
-    const id = navigation.getParam('id')
+    const id = navigation.getParam('id');
     const {state,editBlogPost} = useContext(Context)
-    const blogPost = state.find(blogPost=> blogPost.id == id)
+    const blogPost = state.find(blogPost => blogPost.id == id )
     
     return <BlogPostForm 
     intitialValues={{ title: blogPost.title, content: blogPost.content }}
     onSubmit={(title, content) => {
-        editBlogPost(id,title,content)
+        editBlogPost(id,title,content, ()=> navigation.pop())
+        console.log( title,content,id)
     }}/>
 
 
 };
 
-const styles = StyleSheet.create({});
 
+
+
+const styles = StyleSheet.create({});
 
 export default EditScreen;
